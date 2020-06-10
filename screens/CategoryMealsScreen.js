23432a90a2428d70f65624ca6ec03c4134f81031
Archi Chester <me@qz0.ru>
2020-06-10 22:51:34 +0300

    Add StackNavigation

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const CategoryMealsScreen = (props) => {
+const CategoryMealScreen = (props) => {
     return (
         <View style={styles.screen}>
-            <Text>The Category Meals Screen!</Text>
+            <Text>The Category Meal Screen!</Text>
         </View>
     );
 };
@@ -17,4 +17,4 @@ const styles = StyleSheet.create({
     },
 });
 
-export default CategoryMealsScreen;
+export default CategoryMealScreen;
