b28a022e071964ee390c74eb8a3c75d9b7c0bcab
Archi Chester <me@qz0.ru>
2020-06-12 22:28:13 +0300

    add quad

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,26 +2,24 @@ import React from 'react';
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
-import Colors from '../constants/Colors';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 const CategoriesScreen = (props) => {
     const renderGridItem = (itemData) => {
         return (
-            <TouchableOpacity
-                style={styles.gridItem}
-                onPress={() => {
+            <CategoryGridTile
+                title={itemData.item.title}
+                color={itemData.item.color}
+                onSelect={() => {
                     props.navigation.navigate({
                         routeName: 'CategoryMeals',
                         params: { categoryId: itemData.item.id },
                     });
                 }}
-            >
-                <View>
-                    <Text>{itemData.item.title}</Text>
-                </View>
-            </TouchableOpacity>
+            />
         );
     };
+
     return (
         <FlatList
             keyExtractor={(item, index) => item.id}
@@ -41,11 +39,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-    },
-    gridItem: {
-        flex: 1,
-        margin: 15,
-        height: 150,
     },
 });
 
